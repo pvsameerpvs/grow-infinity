6ecf5004409b3791f8d11fb5feae22417e171305
pvsameerpvs <pvsameerpvs@gmail.com>
2026-02-07 17:27:53 +0400

    fix: ui

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Hero />
       <TrustBadges />
       <AboutSection/>
-      <Stats />
+      {/* <Stats /> */}
       <WhyChooseUs />
       <CoreServices />
       {/* <BankingAdvantage /> */}
